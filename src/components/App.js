d0c3274db6db830974eed04646854a3a21fb21f0
lema samuel <lemasam10@gmail.com>
2024-05-15 18:35:44 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,32 @@
 // create your App component here
+import React from 'react'
+
+function App() {
+    fetch("https://api.example.com/items")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result.items
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
